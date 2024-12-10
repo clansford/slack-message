cbfd0d8587916f0569e7fc09d0603b87fbdc3ce8
christian lansford <christian.lansford@dexcom.com>
2024-12-10 10:18:24 -0500

    refactor: move some funcs into Cli args struct

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use core::panic;
-use std::env;
-
 use clap::Parser;
+use core::panic;
 use dotenv::dotenv;
 use reqwest::{header, Client};
 use serde::{Deserialize, Serialize};
+use std::env;
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
   dotenv().ok();
   let args = Cli::parse();
-  let channel = get_channel(&args);
-  let bearer_token = get_bearer_token(&args);
-  let slack_message = SlackMessage { channel, text: args.message };
+  let bearer_token = args.get_bearer_token()?;
+  let slack_message =
+    SlackMessage { channel: args.get_channel()?, text: args.message };
   let body = create_request_body(slack_message);
   let resp = Client::new()
     .post("https://slack.com/api/chat.postMessage")
@@ -24,19 +23,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     .await?;
 
   let resp_body = resp.text().await?;
-  let slack_response = parse_res_body(&resp_body);
+  let slack_response = SlackResponse::parse(&resp_body)?;
   if slack_response.ok {
     println!("Message sent successfully");
     Ok(())
   } else {
     panic!("Error: Message not sent successfully");
-  }
-}
-
-fn parse_res_body(body: &str) -> SlackResponse {
-  match serde_json::from_str::<SlackResponse>(body) {
-    Ok(res) => res,
-    Err(e) => panic!("{e}"),
   }
 }
 
@@ -48,28 +40,6 @@ fn create_request_body(slack_message: SlackMessage) -> serde_json::Value {
   }
 }
 
-fn get_channel(cli: &Cli) -> String {
-  if cli.channel.is_some() {
-    return cli.channel.clone().unwrap();
-  }
-  match env::var("SLACK_CHANNEL") {
-    Ok(c) => c,
-    Err(e) => panic!("{e}"),
-  }
-}
-
-fn get_bearer_token(cli: &Cli) -> String {
-  let token = if cli.auth_token.is_some() {
-    cli.auth_token.clone().unwrap()
-  } else {
-    match env::var("SLACK_TOKEN") {
-      Ok(c) => c,
-      Err(e) => panic!("{e}"),
-    }
-  };
-  format!("Bearer {token}")
-}
-
 #[derive(Parser, Debug)]
 struct Cli {
   #[arg(short, long)]
@@ -77,6 +47,36 @@ struct Cli {
   message: String,
   #[arg(short, long)]
   auth_token: Option<String>,
+}
+
+impl Cli {
+  fn get_channel(&self) -> Result<String, Box<dyn Error>> {
+    if self.channel.is_some() {
+      return Ok(self.channel.clone().unwrap());
+    }
+    match env::var("SLACK_CHANNEL") {
+      Ok(c) => Ok(c),
+      Err(e) => {
+        eprintln!("Couldn't find SLACK_CHANNEL\n{e:?}");
+        Err(e.into())
+      }
+    }
+  }
+
+  fn get_bearer_token(&self) -> Result<String, Box<dyn Error>> {
+    let token = if self.auth_token.is_some() {
+      self.auth_token.clone().unwrap()
+    } else {
+      match env::var("SLACK_TOKEN") {
+        Ok(tok) => tok,
+        Err(e) => {
+          eprintln!("Couldn't find SLACK_TOKEN\n{e:?}");
+          return Err(e.into());
+        }
+      }
+    };
+    Ok(format!("Bearer {token}"))
+  }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -91,6 +91,18 @@ struct SlackResponse {
   channel: String,
   ts: String,
   message: Message,
+}
+
+impl SlackResponse {
+  fn parse(s: &str) -> Result<Self, Box<dyn Error>> {
+    match serde_json::from_str::<SlackResponse>(s) {
+      Ok(res) => Ok(res),
+      Err(e) => {
+        eprintln!("Error parsing SlackResponse\n{e:?}");
+        Err(e.into())
+      }
+    }
+  }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
